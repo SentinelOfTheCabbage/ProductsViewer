53b3cdf218c67f02936f46d4329b8cd9f7f0e889
Vitaly <peryatin.vitalik37@gmail.com>
2019-02-02 03:00:54 +0300

    create graph reports

---
+++ Work/Scripts/view/reports/chart_interfaces.py
@@ -0,0 +1,48 @@
+"""
+В модуле находятся все классы и интерфейсы для создания диаграмм
+"""
+from abc import ABC, abstractmethod
+
+
+class IChart(ABC):
+    """Интерфейс для созданияя диаграмм. Содержит в себе заголовок
+    и метод для открытия окна с отчётом. """
+
+    _title = ""
+    _x_title = ""
+    _y_title = ""
+
+    def __init__(self, title):
+        self._title = title
+
+    def set_title(self, title):
+        """Устанавливает заголовок отчёта.
+
+        :param title: заголовок отчёта
+        :return: возвращает себя для реализации паттерна "Builder"
+        """
+        self._title = title
+        return self
+
+    @abstractmethod
+    def show(self):
+        """Создаёт график на основе переданных данных и
+        открывает окно отчёта"""
+
+    def set_x_title(self, x_title: str):
+        """Устанавливает заголовок для оси Ox.
+
+        :param x_title: заголовок для оси Ox
+        :return: возвращает себя же для реализации паттерна "Builder"
+        """
+        self._x_title = x_title
+        return self
+
+    def set_y_title(self, y_title: str):
+        """Устанавливает заголовок для оси Oy.
+
+        :param y_title: заголовок для оси Oy
+        :return: возвращает себя же для реализации паттерна "Builder"
+        """
+        self._y_title = y_title
+        return self
---
+++ Work/Scripts/view/reports/scatter_chart.py
@@ -0,0 +1,91 @@
+"""
+Модуль предоставляет возможность для работы с диаграммой типа
+"Категоризированная диаграмма рассеивания".
+"""
+
+import matplotlib.pyplot as plt
+from Work.Scripts.view.reports.chart_interfaces import IChart
+
+
+class ScatterChart(IChart):
+    """Предоставляет удобный интерфейс для создания окна и диаграммы
+        типа "Категоризированная диаграмма рассеивания" внутри него.
+
+        Для создания качественного отчёта рекомендуется передать список
+        групп продуктов, список цен, список количества проданных продуктов,
+        заголовок диаграммы (рекомендуется дата отсчёта),
+        установить название оси Ox (рекомендуется "Количество") и
+        установить название оси Oy (рекомендуется название "Цена")
+
+        """
+    _x_coord = []
+    _y_coord = []
+    _color = "blue"
+    _marker = "o"
+    _exception_text = "Amount of x-coordinates and " \
+                      "y-coordinates should be equals"
+
+    def __init__(self, title=""):
+        super().__init__(title)
+
+    def set_coord(self, x_coord: list, y_coord: list):
+        """ Устанавливает координаты точек. Сначала задаётся
+        список координат X, после список координат по оси Y.
+
+        :param x_coord: список координат по оси X
+        :param y_coord: список координат по оси Y
+        :return: возвращает себя же для реализации паттерна "Builder"
+        """
+        if len(x_coord) != len(y_coord):
+            raise Exception(self._exception_text)
+        self._x_coord = x_coord
+        self._y_coord = y_coord
+        return self
+
+    def set_points(self, points: list):
+        """ Устанавливает список точек. Передаётся список, состоящий из
+        списков двух значений: координата по X и координата по Y.
+
+        :param points: список точек
+        :return: возвращает себя же для реализации паттерна "Builder"
+        """
+        self._x_coord.clear()
+        self._y_coord.clear()
+        try:
+            for (x, y) in points:
+                self._x_coord.append(x)
+                self._y_coord.append(y)
+        except ValueError:
+            raise Exception(self._exception_text)
+        return self
+
+    def set_color(self, color):
+        """ Устанавливает цвет точек. Цвет по умолчанию: синий.
+
+        :param color: цвет RGB (рекомендуется в формате HEX)
+        :return: возвращает себя же для реализации паттерна "Builder"
+        """
+        self._color = color
+        return self
+
+    def show(self):
+        """Строит и показывает диаграмму рассеивания,
+        исходя из переданных значений.
+
+        При вызове этой функции открывается новое окно с отчётом
+        на основе переданных значений. Если значение не передавалось,
+        то используется значение по умолчанию.
+        """
+        plt.scatter(self._x_coord, self._y_coord, marker=self._marker,
+                    edgecolors=self._color, c=self._color)
+        plt.ylabel(self._y_title)
+        plt.xlabel(self._x_title)
+        plt.show()
+
+
+ScatterChart()\
+    .set_points([[1, 2], [2, 3]])\
+    .set_color("#FF0000")\
+    .set_x_title("Название оси X")\
+    .set_y_title("Название оси Y")\
+    .show()

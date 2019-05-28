65d8d780a9e7ea56b8593389bc19c38f865cf1e8
Виталий Перятин <peryatin.vitalik37@gmail.com>
2019-05-28 12:09:48 +0300

    Correct structure

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
 
-from Work.Scripts.src.presentation.main_window.views.main_window import MainWindow
 from tkinter import Tk
 from Work.Scripts import conf
+from Work.Scripts.main_window import MainWindow
 
-f = open(conf.ROOT_DIR + r'\Data\Temp\filename.txt', 'w')
+f = open(conf.ROOT_DIR + r'\Data\filename.txt', 'w')
 f.write(conf.ROOT_DIR + r'\Data\db.pickle')
 f.close()
 sys.path.insert(0, "D:\\Python projects\\ProductsViewer\\Work\\Scripts")
